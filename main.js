8c0fe507dcb3e7651364842a2c0ba8c6e1663e6b
obed300 <obamfo@st.ug.edu.gh>
2022-10-17 12:31:25 +0000

    Add Javascript file with content

---
+++ main.js
@@ -0,0 +1,9 @@
+let buttoms = document.querySelector('.buttoms');
+let menu = document.querySelector('.menu-list');
+
+
+ 
+buttoms.addEventListener('click',function() {
+    if(menu.style.display='block')
+    menu.style.display= 'block';
+});
